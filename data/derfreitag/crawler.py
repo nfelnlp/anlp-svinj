3b85ab7bf851fbbe26f041b04d131a81bbc64e26
himbiss <vincent.ortland@gmx.de>
2018-01-30 00:13:17 +0100

    changed row format of freitag.de files

--- data/derfreitag/crawler.py
+++ data/derfreitag/crawler.py
@@ -16,6 +16,13 @@ def bp_extract(url):
     extr = Extractor(extractor='ArticleExtractor', url=url)
     return extr.getText()
 
+def prepare_authors(authors):
+    string = "['" + authors[0] + "'"
+    for author in authors[1:]:
+        string += ",'" + author + "'"
+    string += "]"
+    return string
+
 if __name__ == "__main__":
     counter = 1
     d_str = '/home/himbiss/news-please-repo/data/2018/01/24/freitag.de'
@@ -23,19 +30,23 @@ if __name__ == "__main__":
         if f_path.endswith('.json'):
             with open(os.path.join(d_str, f_path)) as f:
                 d = json.load(f)
-                try:
-                    text = bp_extract(d['url'])
-                    path = 'data/'+str(counter)+'.txt'
-                    with open(path, 'w+') as u:
-                        with open('derfreitag.csv', 'a+') as csvfile:
-                            print(d['url'])
-                            writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                            sent_tokenize_list = sent_tokenize(text, language='german')
-                            for sent in sent_tokenize_list:
-                                u.write(str(sent)+'\n')
-                            ts = time.time()
-                            ts_format = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-                            writer.writerow([path, d['url'], ts_format])
-                            counter += 1
-                except socket.timeout:
-                    print('Caught socket timeout!')
+                if d['date_publish']:
+                    try:
+                        text = bp_extract(d['url'])
+                        path = 'data/'+str(counter)+'.txt'
+                        with open(path, 'w+') as u:
+                            with open('derfreitag.csv', 'a+') as csvfile:
+                                print(d['url'])
+                                publish_date = d['date_publish'].split()[0]
+                                authors = prepare_authors(d['authors'])
+                                categories = 'None'
+
+                                writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                                sent_tokenize_list = sent_tokenize(text, language='german')
+                                for sent in sent_tokenize_list:
+                                    u.write(str(sent)+'\n')
+
+                                writer.writerow([path, d['url'], publish_date, authors, categories])
+                                counter += 1
+                    except socket.timeout:
+                        print('Caught socket timeout!')
