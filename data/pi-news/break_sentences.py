a7b9355abe722572dd4c287e01929f61ac9084ea
Johann Seltmann <johann.seltmann@gmx.net>
2018-02-23 14:17:03 +0100

    added line breaks for pi-news

---
+++ data/pi-news/break_sentences.py
@@ -0,0 +1,11 @@
+from nltk.tokenize import sent_tokenize
+import os
+
+append_break = lambda x : x + "\n"
+
+for art_file in os.listdir('./pi_texts'):
+    with open(os.path.join("./pi_texts", art_file), "r") as article:
+        text = "".join(list(map(append_break, sent_tokenize(article.read()))))
+    with open(os.path.join("./pi_texts", art_file), "w") as article:
+        article.write(text)
+    
