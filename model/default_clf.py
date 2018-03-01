425a7f2a5986fb8fcdcba62ba5347e60faa167e0
slatenf <nilsfeldhus@gmail.com>
2018-03-01 13:36:34 +0100

    Added POS tag feature. Restructured default clf.

--- model/default_clf.py
+++ model/default_clf.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 from sklearn import metrics
@@ -6,68 +5,63 @@ from sklearn import tree
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.neural_network import MLPClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC
 from sklearn.utils import shuffle
 
 import baseline as base
 
+from features import pos_tags
 
-def concatenate_articles(subset, sid):
+
+def build_subset(subset, sid, split):
+    # Read all files of a subset
     df = pd.DataFrame()
-
     files = [row[0] for row in subset]
     articles = [open(f).read() for f in files]
 
     df['text'] = articles
-
     df['file'] = files
-    
-    # INSERT FEATURES HERE
-    # TODO
-
-    if sid == 0:
-        df['label'] = 0
-    else:
-        df['label'] = 1
+    df['label'] = sid
+    df['split'] = split
 
     return df
 
 
-def build_corpus(left, right):
-    data_left = concatenate_articles(left, sid=0)
-    data_right = concatenate_articles(right, sid=1)
+def build_corpus(train_L, train_R, test_L, test_R):
+    train_L = build_subset(train_L, sid="0", split="train")
+    train_R = build_subset(train_R, sid="1", split="train")
+    test_L = build_subset(test_L, sid="0", split="test")
+    test_R = build_subset(test_R, sid="1", split="train")
 
-    # Combine both corpora and shuffle it
-    data = pd.concat([data_left, data_right])
-    shuffled_data = shuffle(data).reset_index(drop=True)
+    # Combine all corpora and shuffle it
+    data = pd.concat([train_L, train_R, test_L, test_R])
+    data = shuffle(data).reset_index(drop=True)
 
-    X = shuffled_data[['text', 'file']]
-    y = shuffled_data['label']
+    return data
 
+
+def split_X_and_y(data):
+    X = data.drop(['label'], axis=1)
+    y = data['label']
     return X, y
 
 
-if __name__ == "__main__":
-    # Parse the datasets
-    left_wing_train = base.read_csv('../data/train/left_wing_train.csv')
-    right_wing_train = base.read_csv('../data/train/right_wing_train.csv')
+def train(data, clf):
+    # Untangle data
+    train_data = data[data['split'] == "train"]
+    test_data = data[data['split'] == "test"]
 
-    left_wing_test = base.read_csv('../data/test/left_wing_test.csv')
-    right_wing_test = base.read_csv('../data/test/right_wing_test.csv')
+    X_train, y_train = split_X_and_y(train_data)
+    X_test, y_test = split_X_and_y(test_data)
 
-    X_train, y_train = build_corpus(left_wing_train, right_wing_train)
-    X_test, y_test = build_corpus(left_wing_test, right_wing_test)
+    # Unselect features from X
+    X_train = X_train.drop(['text', 'file', 'split'], axis=1).fillna(0)
+    X_test = X_test.drop(['text', 'file', 'split'], axis=1).fillna(0)
 
     # Training
-    text_clf = Pipeline([('vect', TfidfVectorizer()),
-                         #('tfidf', TfidfTransformer()),
-                         ('clf', SGDClassifier())])
-    text_clf.fit(X_train.text, y_train)
+    clf.fit(X_train, y_train)
 
     # Apply trained model on test set
-    predicted = text_clf.predict(X_test.text)
+    predicted = clf.predict(X_test)
 
     # Evaluation
     print(metrics.classification_report(y_test, predicted))
@@ -79,3 +73,28 @@ if __name__ == "__main__":
 
     # Print wrong predictions
     print(results[results.pred != results.label])
+
+
+if __name__ == "__main__":
+    # Parse the datasets
+    left_wing_train = base.read_csv('../data/train/left_wing_train.csv')
+    right_wing_train = base.read_csv('../data/train/right_wing_train.csv')
+
+    left_wing_test = base.read_csv('../data/test/true_left_wing_test.csv')
+    right_wing_test = base.read_csv('../data/test/true_right_wing_test.csv')
+
+    #left_wing_train = base.read_csv('../data/train/left_wing_train_short.csv')
+    #right_wing_train = base.read_csv('../data/train/right_wing_train_short.csv')
+
+    #left_wing_test = base.read_csv('../data/test/left_wing_test_short.csv')
+    #right_wing_test = base.read_csv('../data/test/right_wing_test_short.csv')
+
+    data = build_corpus(left_wing_train, right_wing_train,
+                        left_wing_test, right_wing_test)
+
+    # Add features
+    data = pos_tags.add_features(df=data)  # POS tag feature
+
+    # Select classifier and train
+    clf = tree.DecisionTreeClassifier()
+    train(data, clf=clf)
---
+++ model/features/pos_tags.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import treetaggerwrapper
+
+from collections import defaultdict
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction import DictVectorizer
+
+
+def add_features(df):
+    # Call treetagger
+    tagger = treetaggerwrapper.TreeTagger(TAGLANG='de')
+
+    # Retrieve text data from dataframe
+    articles = df["text"].tolist()
+
+    # Create separate dataframe for all POS features
+    pos_df = pd.DataFrame(annotate_with_pos(articles, tagger))
+
+    # Concatenate the two dataframes
+    df = pd.concat([df, pos_df], axis=1)
+
+    return df
+
+
+def annotate_with_pos(articles, tagger):
+    all_pos = []
+
+    for art in articles:
+        tokenized_art = []
+        for sent in art.split('\n'):
+            tokenized_art += word_tokenize(sent)
+        tg_input = "\n".join([t for t in tokenized_art])
+
+        tg_output = tagger.tag_text(tg_input)
+
+        anno = treetaggerwrapper.make_tags(tg_output, exclude_nottags=True)
+
+        pos_dict = defaultdict(list)
+        for a in anno:
+            pos_dict[a[0]] = a[1]
+        all_pos.append(pos_dict)
+
+    vec = DictVectorizer()
+    pos_vectorized = vec.fit_transform(all_pos)
+
+    return pos_vectorized.toarray()
