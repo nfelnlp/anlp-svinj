62bc4567215bdeaf600e2b722e7830e87fa7a393
izzbizz <inxy@protonmail.com>
2018-03-02 20:34:58 +0100

    Added ngrams and quoted speech to features

---
+++ model/features/quoted.py
@@ -0,0 +1,22 @@
+import baseline as base
+import re
+import pandas as pd
+
+def add_features(df):
+	quoted_ratios=[]
+	articles=df["text"].tolist()
+	for article in articles:
+		text_length=len(article.split(" "))
+		quoted=re.findall(r'("[\W\w]+?"|„[\W\w]+?“|»[\W\w]+?«|«[\W\w]+?»)',article)
+		length_quoted=0
+		for item in quoted:
+#			print(item)
+			length=len(item.split(" ")) 
+			if length > 2:
+				length_quoted+=length
+		quoted_ratio=length_quoted/text_length
+		quoted_ratios.append(quoted_ratio)
+	
+	df["quoted_speech"]=pd.Series(quoted_ratios)
+	
+	return df
